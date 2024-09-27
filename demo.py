c088ab9c4591bf9e1f81c241fd378ae5dd098016
yuvrajsinghchauhan <ychauhan191@gmail.com>
2024-09-27 16:46:37 +0530

    demo file updated

---
+++ demo.py
@@ -0,0 +1,9 @@
+# from us_visa.logger import logging
+# from us_visa.exception import USvisaException
+# import sys
+# logging.info("Welcome to the club")
+
+# try:
+#     a = 2/0
+# except Exception as e:
+#     raise USvisaException(e, sys)
